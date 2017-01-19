f8647b93dae04ec2ecfcf79ced08dffb229e066b
JennyPash <jenqpashova@gmail.com>
2017-01-19 16:24:45 +0200

    add more subjects to choose from

--- MyHome/app/src/main/java/com/jenny/myhome/SubjectType.java
+++ MyHome/app/src/main/java/com/jenny/myhome/SubjectType.java
@@ -13,7 +13,19 @@ public enum SubjectType {
     CHAIR(R.string.chair),
     SOFA(R.string.sofa),
     DESK(R.string.desk),
-    UNIT(R.string.unit);
+    UNIT(R.string.unit),
+    FRIDGE(R.string.fridge),
+    OVEN(R.string.oven),
+    DISHWASHER(R.string.dishwasher),
+    ABSORBER(R.string.absorber),
+    DRYING_MACHINE(R.string.drying_machine),
+    TV(R.string.tv),
+    CLIMATIC(R.string.climatic),
+    COUCH(R.string.couch),
+    CARPET(R.string.carpet),
+    PARQUET(R.string.parquet),
+    TILES(R.string.tiles),
+    PORTMANTEAU(R.string.portmanteau);
 
 
     private int resourceId;
